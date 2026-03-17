158fc5545e21705f1697b34bb7ee8e9ddcad07b0
fheberle0 <fred.heberle@gmail.com>
2026-03-17 11:24:41 +0000

    Deploying to gh-pages from @ fheberle0/heberlelab@634d2f62685e825f6a2bbf57b0ada1cb33d9dacb 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,13 +16,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/heberlelab/research/";
           },
-        },{id: "nav-blog",
-          title: "blog",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/heberlelab/blog/";
-          },
         },{id: "nav-domains",
           title: "Domains",
           description: "Research domains and focus areas of the Heberle Lab",
@@ -44,27 +37,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/heberlelab/people/";
           },
-        },{id: "nav-projects",
-          title: "projects",
-          description: "A growing collection of your cool projects.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/heberlelab/projects/";
-          },
-        },{id: "nav-repositories",
-          title: "repositories",
-          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/heberlelab/repositories/";
-          },
-        },{id: "nav-cv",
-          title: "CV",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/heberlelab/cv/";
-          },
         },{id: "nav-friends",
           title: "Friends",
           description: "Collaborators and friends of the Heberle Lab",
@@ -72,35 +44,7 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/heberlelab/friends/";
           },
-        },{id: "nav-teaching",
-          title: "teaching",
-          description: "Course materials, schedules, and resources for classes taught.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/heberlelab/teaching/";
-          },
-        },{id: "nav-people",
-          title: "people",
-          description: "members of the lab or group",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/heberlelab/people/";
-          },
-        },{id: "dropdown-bookshelf",
-              title: "bookshelf",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/heberlelab/books/";
-              },
-            },{id: "dropdown-blog",
-              title: "blog",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/heberlelab/blog/";
-              },
-            },{id: "post-a-post-with-plotly-js",
+        },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
         
